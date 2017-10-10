9a68655180e816d50442aca52f594dfe7e74a1e9
Ibrar <ibrar.mumtaz@agepartnership.com>
2017-10-10 11:25:47 +0100

    Added in support for Swagger XML Comments - none atm to display - will be added in a later check-in.

--- src/Services/Experimentation/Experimentation.Api/StartupExtensions.cs
+++ src/Services/Experimentation/Experimentation.Api/StartupExtensions.cs
@@ -1,10 +1,12 @@
-﻿using Autofac;
+﻿using System.IO;
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.Swagger.Model;
 using Experimentation.Configuration.Ioc;
 using Experimentation.Persistence.Ioc;
 using Experimentation.Logic.Ioc;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Experimentation.Api
 {
@@ -24,6 +26,10 @@ namespace Experimentation.Api
                     Description = "A Rest service that provides http responses to feature string requests.",
                     TermsOfService = "To be used by Age and Pure code only"
                 });
+
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "Experimentation.Api.xml");
+                options.IncludeXmlComments(xmlPath); ;
             });
             return services;
         }
